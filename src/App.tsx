066a6d57f219a1e8c1e33c7c3cf6b818f2f2d854
Caio Labella <caiolabella@outlook.com>
2024-08-15 21:11:40 -0300

    implementado global css e thema no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { ThemeContextProvider } from "./contexts/ThemeContext";
+import { Routers } from "./routes";
+import GlobalStyles from "./styles/global"
+
 function App() {
   return (
-    <></>
+    <ThemeContextProvider>
+      <GlobalStyles />
+      <Routers />
+    </ThemeContextProvider>
   );
 }
 
